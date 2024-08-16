41e61f67938595e39a2203ba2998877f5b3e19db
rk <ranjitkale0001@gmail.com>
2024-08-16 15:32:42 +0530

    service components are created

---
+++ src/app/service/emp.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpService {
+
+  constructor() { }
+
+  getEmpList(){
+    return[
+      {"eno":100, "ename":"Sachin", "salary":15000},
+      {"eno":102, "ename":"Ranjit", "salary":65000},
+      {"eno":103, "ename":"Jayraj", "salary":90000},
+    ]
+  }
+}
