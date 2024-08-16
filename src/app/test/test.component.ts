f34904ad23a63c1b34f5f8721e76129bf57ac780
rk <ranjitkale0001@gmail.com>
2024-08-16 15:58:34 +0530

    ngModel [()] data chage in line

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
