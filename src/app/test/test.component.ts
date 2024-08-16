5ccad0a425045aa67d17cf27edd9f83c7c3ffb04
rk <ranjitkale0001@gmail.com>
2024-08-16 15:21:40 +0530

    Data binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
   uname:string="Ranjit Kale" 
+  flag1:boolean=true;
+  flag2:boolean=false;
 
   constructor(){
 
